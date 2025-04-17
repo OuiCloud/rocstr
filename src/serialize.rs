9a2820971461e27549dc3f30872093e0c32c5e8f
Philippe Menuet Tang <philippe@ouicloud.software>
2025-04-17 12:30:26 +0200

    fix: remove RocStr unecessary deserialize functions

--- src/serialize.rs
+++ src/serialize.rs
@@ -48,27 +48,6 @@ mod no_std_rocstr {
             }
         }
 
-        fn visit_i8<E>(self, v: i8) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_i16<E>(self, v: i16) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
         fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
         where
             E: serde::de::Error,
@@ -77,27 +56,6 @@ mod no_std_rocstr {
         }
 
         fn visit_i128<E>(self, v: i128) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_u8<E>(self, v: u8) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            self.visit_u64(v as u64)
-        }
-
-        fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
         where
             E: serde::de::Error,
         {
@@ -172,27 +130,6 @@ mod standard_rocstr {
             }
         }
 
-        fn visit_i8<E>(self, v: i8) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_i16<E>(self, v: i16) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_i32<E>(self, v: i32) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
         fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
         where
             E: serde::de::Error,
@@ -201,27 +138,6 @@ mod standard_rocstr {
         }
 
         fn visit_i128<E>(self, v: i128) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_u8<E>(self, v: u8) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            self.visit_u64(v as u64)
-        }
-
-        fn visit_u16<E>(self, v: u16) -> Result<Self::Value, E>
-        where
-            E: serde::de::Error,
-        {
-            Ok(RocStr::from(v).reshape::<SIZE>())
-        }
-
-        fn visit_u32<E>(self, v: u32) -> Result<Self::Value, E>
         where
             E: serde::de::Error,
         {
